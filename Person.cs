35baf92360e87be399187b5203a37b397b687324
Philip Seisman <Philip.seisman@gmail.com>
2024-11-14 15:05:54 -0500

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCAD16_Assignment2_3_1_SR
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public double Age { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Xml;
+
+namespace CCAD16_Assignment2_3_1_SR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFile(@"Test.txt");
+            ReadFile(@"Test.txt");
+        }
+
+        static public void ReadFile(string path)
+        {
+            string filePath = @"Test.txt";
+            string line;
+            StreamReader sr = new StreamReader(filePath);
+            line = sr.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            Console.ReadLine();             
+        }
+
+        static public void WriteFile(string path)
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine("Hello World");
+            sw.WriteLine("This is an additional line.");
+            sw.Close();
+        }
+    }
+}
